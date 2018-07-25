f889a4ed7114e782b4a85283585f570852af2d1e
cJieYang <sjieyang@163.com>
2018-07-25 10:25:58 +0800

    add sql

--- neusoft/WebRoot/static/js/login.js
+++ neusoft/WebRoot/static/js/login.js
@@ -1,8 +1,8 @@
 const button = _prime(".login_button");
 
-button.addEventListener("click",()={
-	ajax({
-		type:"post",
-		timeOut:5000
-	});
-});
+//button.addEventListener("click",()={
+//	ajax({
+//		type:"post",
+//		timeOut:5000
+//	});
+//});
