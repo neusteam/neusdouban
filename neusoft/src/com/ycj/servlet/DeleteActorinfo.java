6b8b204bce5877acf0f1def0c332fd278051a851
cJieYang <sjieyang@163.com>
2018-07-26 19:28:12 +0800

    actor function

---
+++ neusoft/src/com/ycj/servlet/DeleteActorinfo.java
@@ -0,0 +1,87 @@
+package com.ycj.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.zzh.bean.ActorEntity;
+import com.zzh.dao.ActorEntityDAO;
+@WebServlet("/DeleteActorinfo")
+public class DeleteActorinfo extends HttpServlet {
+
+	/**
+		 * Constructor of the object.
+		 */
+	public DeleteActorinfo() {
+		super();
+	}
+
+	/**
+		 * Destruction of the servlet. <br>
+		 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+		 * The doGet method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to get.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+	
+
+	/**
+		 * The doPost method of the servlet. <br>
+		 *
+		 * This method is called when a form has its tag value method equals to post.
+		 * 
+		 * @param request the request send by the client to the server
+		 * @param response the response send by the server to the client
+		 * @throws ServletException if an error occurred
+		 * @throws IOException if an error occurred
+		 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		int id=Integer.parseInt(request.getParameter("id"));
+		ActorEntity actor=new ActorEntity();
+		actor.setId(id);
+		ActorEntityDAO a=new ActorEntityDAO();
+		boolean flag=a.deleteActor(actor);
+		if(flag){
+			System.out.println("É¾³ý³É¹¦");
+			
+		}
+		else{
+			System.out.println("É¾³ýÊ§°Ü");
+		}
+		
+		out.flush();
+		out.close();
+	}
+
+	/**
+		 * Initialization of the servlet. <br>
+		 *
+		 * @throws ServletException if an error occurs
+		 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
---
+++ neusoft/src/com/zzh/bean/ActorEntity.java
@@ -0,0 +1,60 @@
+package com.zzh.bean;
+
+public class ActorEntity {
+private int id;
+private String name;
+private String sex;
+private String birth;
+private String hometown;
+private String occupation;
+private String describe;
+private String url;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getSex() {
+	return sex;
+}
+public void setSex(String sex) {
+	this.sex = sex;
+}
+public String getBirth() {
+	return birth;
+}
+public void setBirth(String birth) {
+	this.birth = birth;
+}
+public String getHometown() {
+	return hometown;
+}
+public void setHometown(String hometown) {
+	this.hometown = hometown;
+}
+public String getOccupation() {
+	return occupation;
+}
+public void setOccupation(String occupation) {
+	this.occupation = occupation;
+}
+public String getDescribe() {
+	return describe;
+}
+public void setDescribe(String describe) {
+	this.describe = describe;
+}
+public String getUrl() {
+	return url;
+}
+public void setUrl(String url) {
+	this.url = url;
+}
+}
