b14b8a9357c5e360dddf8f24fa246e5627bf91e8
cJieYang <sjieyang@163.com>
2018-07-30 10:48:30 +0800

    2018-07-30 showmovie

--- neusoft/src/com/zzh/bean/MovieEntity.java
+++ neusoft/src/com/zzh/bean/MovieEntity.java
@@ -1,41 +1,28 @@
 package com.zzh.bean;
 
 public class MovieEntity {
- private int id;
- private float score;
- private String comments;
- private int userId;
- private int movieId;
+ private int movie_id;
+ private String movieName;
  private String date;
-public int getId() {
-	return id;
+ private String director;
+ private String actor;
+ private String type;
+ private String country;
+ private String language;
+ private String picture;
+ private float average;
+ private String describe;
+public int getMovie_id() {
+	return movie_id;
 }
-public void setId(int id) {
-	this.id = id;
+public void setMovie_id(int movie_id) {
+	this.movie_id = movie_id;
 }
-public float getScore() {
-	return score;
+public String getMovieName() {
+	return movieName;
 }
-public void setScore(float score) {
-	this.score = score;
-}
-public String getComments() {
-	return comments;
-}
-public void setComments(String comments) {
-	this.comments = comments;
-}
-public int getUserId() {
-	return userId;
-}
-public void setUserId(int userId) {
-	this.userId = userId;
-}
-public int getMovieId() {
-	return movieId;
-}
-public void setMovieId(int movieId) {
-	this.movieId = movieId;
+public void setMovieName(String movieName) {
+	this.movieName = movieName;
 }
 public String getDate() {
 	return date;
@@ -43,4 +30,78 @@ public String getDate() {
 public void setDate(String date) {
 	this.date = date;
 }
+public String getDirector() {
+	return director;
+}
+public void setDirector(String director) {
+	this.director = director;
+}
+public String getActor() {
+	return actor;
+}
+public void setActor(String actor) {
+	this.actor = actor;
+}
+public String getType() {
+	return type;
+}
+public void setType(String type) {
+	this.type = type;
+}
+public String getCountry() {
+	return country;
+}
+public void setCountry(String country) {
+	this.country = country;
+}
+public String getLanguage() {
+	return language;
+}
+public void setLanguage(String language) {
+	this.language = language;
+}
+public String getPicture() {
+	return picture;
+}
+public void setPicture(String picture) {
+	this.picture = picture;
+}
+public float getAverage() {
+	return average;
+}
+public void setAverage(float average) {
+	this.average = average;
+}
+public String getDescribe() {
+	return describe;
+}
+public void setDescribe(String describe) {
+	this.describe = describe;
+}
+public MovieEntity(int movie_id, String movieName, String date, String director, String actor, String type,
+		String country, String language, String picture, float average, String describe) {
+	super();
+	this.movie_id = movie_id;
+	this.movieName = movieName;
+	this.date = date;
+	this.director = director;
+	this.actor = actor;
+	this.type = type;
+	this.country = country;
+	this.language = language;
+	this.picture = picture;
+	this.average = average;
+	this.describe = describe;
+}
+public MovieEntity() {
+	super();
+}
+@Override
+public String toString() {
+	return "{\"movie_id\":\"" + movie_id + "\",\"movieName\":\"" + movieName + "\",\"date\":\"" + date
+			+ "\",\"director\":\"" + director + "\",\"actor\":\"" + actor + "\",\"type\":\"" + type
+			+ "\",\"country\":\"" + country + "\",\"language\":\"" + language + "\",\"picture\":\"" + picture
+			+ "\",\"average\":\"" + average + "\",\"describe\":\"" + describe + "\"}";
+}
+
 }
---
+++ neusoft/src/com/zzh/dao/MovieEntityDAO.java
@@ -0,0 +1,66 @@
+package com.zzh.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.zzh.bean.ActorEntity;
+import com.zzh.bean.MovieEntity;
+import com.zzh.util.DBConnection;
+
+public class MovieEntityDAO {
+	public ArrayList<MovieEntity> getMovieList(MovieEntity me)
+	{
+		
+			ArrayList<MovieEntity> movielist = new ArrayList<MovieEntity>();  
+			Statement sta = null;
+			Connection conn = null;
+			ResultSet rs = null;
+	        try {
+				conn = DBConnection.getConnection();  
+		        sta = conn.createStatement();  		        
+		        String sql = "select * from movieinfo where moviename='"+me.getMovieName()+"'";
+		        System.out.println(sql);
+		        rs = sta.executeQuery(sql); 		      
+		        while (rs.next()){ 
+		        	
+
+		        	int id=rs.getInt("movie_id");
+		        	String moviename=rs.getString("moviename");
+		            String date=rs.getString("date"); 
+		            String director=rs.getString("director"); 
+		            String actor=rs.getString("actor"); 
+		            String type=rs.getString("type"); 
+		            String country=rs.getString("country"); 
+		            String language=rs.getString("language"); 
+		            String picture=rs.getString("picture");
+		            float average=rs.getFloat("average");
+		            String describe=rs.getString("moviedescribe");
+		            MovieEntity movie=new MovieEntity(id,moviename,date,director,actor,type,country,language,picture,average,describe);
+		            movielist.add(movie);
+		            System.out.println("信息为："+movielist);
+		        
+		        }  
+			} 
+	        catch (SQLException e){
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				
+			}  finally
+			{
+				DBConnection.close(conn, sta);
+			}
+	        return movielist;
+		}
+
+	public static void main(String[] args) {
+		MovieEntity me=new MovieEntity();
+		me.setMovieName("我不是药神");
+		ArrayList<MovieEntity> movielist = new ArrayList<MovieEntity>();
+		MovieEntityDAO m=new MovieEntityDAO();
+		movielist=m.getMovieList(me);
+		System.out.println("信息3"+movielist);
+	}
+}
